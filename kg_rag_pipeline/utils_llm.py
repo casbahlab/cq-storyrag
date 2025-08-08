a933c6af690c3da71452eae8e67f5cf0aeed6f54
sowjanyab <sowju89@gmail.com>
2025-08-09 00:28:46 +0100

    feat: Needed to increase the input token limit to support the large prompts

--- kg_rag_pipeline/utils_llm.py
+++ kg_rag_pipeline/utils_llm.py
@@ -1,7 +1,7 @@
 # utils_llm.py
 import requests
 OLLAMA_HOST = "http://127.0.0.1:11434"
-MODEL = "llama3.1"  # change to your local model tag
+MODEL = "llama3.1-128k"  # change to your local model tag
 
 def ollama_chat(prompt: str) -> str:
     r = requests.post(f"{OLLAMA_HOST}/api/generate", json={
